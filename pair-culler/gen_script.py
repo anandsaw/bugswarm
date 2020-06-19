b8ac12c80a959feb13ca310d139c84b5f5d2cf41
Anand Sawant <anandsaw@gmail.com>
2020-06-19 13:28:30 -0700

    make travis execution absolute.

--- pair-culler/gen_script.py
+++ pair-culler/gen_script.py
@@ -7,7 +7,7 @@ def gen_script(build_sh_path, job_number, reproducing_dir):
     """
     Invoke travis-build to generate the build script.
     """
-    travis_command = '~/.travis/travis-build/bin/travis compile {} > {}'.format(job_number, build_sh_path)
+    travis_command = '/home/anandsaw/.travis/travis-build/bin/travis compile {} > {}'.format(job_number, build_sh_path)
     print("Creating travis build script with command: " + travis_command)
     cd_command = 'cd {}'.format(reproducing_dir)
     _, stderr, returncode = ShellWrapper.run_commands(cd_command, travis_command,
